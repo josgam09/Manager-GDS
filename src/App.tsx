b4cb7660a31d1cd2a313c7509f19ecf04a5bd2bb
Jose Gamez <jose.gamez@jetsmart.com>
2025-10-23 13:57:21 -0300

    Dar acceso al usuario Supervisor para crear nuevos requerimientos

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => (
                         <Route 
                           path="/requirements/new" 
                           element={
-                            <ProtectedRoute allowedRoles={['ADMINISTRADOR', 'ANALISTA']}>
+                            <ProtectedRoute allowedRoles={['ADMINISTRADOR', 'ANALISTA', 'SUPERVISOR']}>
                               <RequirementForm />
                             </ProtectedRoute>
                           } 
